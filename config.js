a874a53a74a2e01159a97413520e66ee082273f3
Gabriel <gabrielenriquecas@gmail.com>
2023-08-28 09:58:05 -0400

    '/' removed and fixed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PAGE_URL = process.env.NODE_ENV === 'production' ? 'https://todoapp-yio0.onrender.com/' : 'http://localhost:3000';
+const PAGE_URL = process.env.NODE_ENV === 'production' ? 'https://todoapp-yio0.onrender.com' : 'http://localhost:3000';
 const MONGO_URI = process.env.NODE_ENV === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_TEST;
 
 module.exports = { PAGE_URL, MONGO_URI };
