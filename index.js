a7bcce4c5aede6b2764926949c60f621a994031f
Gabriel <gabrielenriquecas@gmail.com>
2023-08-29 16:24:21 -0400

    consolelog removed

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = http.createServer(app);
 
 server.listen(3000, () => {
     console.log('El servidor está corriendo en el puerto 3000');
-    console.log('http://localhost:3000/');
+    // console.log('http://localhost:3000/');
 });
