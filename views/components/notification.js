a3d888b09cc8a3e4f579cca80ab33e4e0b93414e
Gabriel <gabrielenriquecas@gmail.com>
2023-08-31 20:42:26 -0400

    linkbtn removed, notification fixed

--- views/components/notification.js
+++ views/components/notification.js
@@ -4,13 +4,13 @@ export const createNotification = (isError, message) => {
     if (isError) {
         div.innerHTML = `
         <div class="max-w-7xl mx-auto px-4 flex justify-end">
-            <p class="bg-red-500 p-4 w-3/12 rounded-lg font-bold">${message}</p>
+            <p class="bg-red-500 p-4w-2/4 md:w-3/12 rounded-lg font-bold">${message}</p>
         </div>
         `;
     } else {
         div.innerHTML = `
         <div class="max-w-7xl mx-auto px-4 flex justify-end">
-            <p class="bg-green-400 p-4 w-3/12 rounded-lg font-bold">${message}</p>
+            <p class="bg-green-400 p-4 w-2/4 md:w-3/12 rounded-lg font-bold">${message}</p>
         </div>
         `;
     }
